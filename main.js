b076754968225d561518d285307281fa40cdd3d7
rodiane <radion.yu@gmail.com>
2020-10-15 14:15:00 +0500

    complete scripts

--- main.js
+++ main.js
@@ -1,2 +1,19 @@
-let numberOfFilms = prompt("Сколько фильмов посомтрел?");
-console.log(numberOfFilms)
+let personalMoviesDb;
+
+let numberOfFilms = prompt("How many films you watch?");
+let watchedFilms = prompt("One more watched films?");
+let raitWatcheFilm = prompt("Rait watched film?");
+
+
+personalMoviesDb = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+personalMoviesDb.movies[watchedFilms] = raitWatcheFilm;
+
+
+console.log(personalMoviesDb);
