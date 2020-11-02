c1fbee801fc569cc4f59f83b93ee6fdd9858b481
rodiane <radion.yu@gmail.com>
2020-11-02 15:59:03 +0500

    task 3

--- main.js
+++ main.js
@@ -1,12 +1,2 @@
-let age = null;
 
-switch(age) {
-    case 40:
-        console.log("True");
-        break;
-    case 25:
-        console.log("false");
-        break;
-    default:
-        console.log("Пох!");
-}
+console.log(0 || "" || 2 || undefined || true || falsе);
